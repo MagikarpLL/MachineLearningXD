6b3e194911a57eddd66383aa05b3802244ad4b46
wangluo <3422785094@qq.com>
2018-03-09 17:35:27 +0800

    1

---
+++ KNN/2.2.1Dating/Writing_KNN.py
@@ -0,0 +1,13 @@
+# coding:utf-8
+from numpy import *
+
+
+# 将32*32存储的图像转为1*1024的向量
+def img2vector(filename):
+    returnVect = zeros((1, 1024))
+    fr = open(filename)
+    for i in range(32):
+        lineStr = fr.readline()
+        for j in range(32):
+            returnVect[0, 32 * i + j] = int(lineStr[j])
+    return returnVect
