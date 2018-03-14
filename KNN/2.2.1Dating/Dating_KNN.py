037ee822301f490803976b4d4f6a13f29968e962
magikarpll <3422785094@qq.com>
2018-03-14 23:54:27 +0800

    3

--- KNN/2.2.1Dating/Dating_KNN.py
+++ KNN/2.2.1Dating/Dating_KNN.py
@@ -83,4 +83,4 @@ def classifyPerson():
 
 
 # datingClassTest()
-classifyPerson()
+#classifyPerson()
--- KNN/2.2.1Dating/Writing_KNN.py
+++ KNN/2.2.1Dating/Writing_KNN.py
@@ -1,6 +1,7 @@
 # coding:utf-8
 from os import listdir
 from numpy import *
+from Dating_KNN import *
 
 
 # 将32*32存储的图像转为1*1024的向量
@@ -13,20 +14,32 @@ def img2vector(filename):
             returnVect[0, 32 * i + j] = int(lineStr[j])
     return returnVect
 
-#手写数字识别系统的测试代码
+
+# 手写数字识别系统的测试代码
 def handwritingClassTest():
     hwLabels = []
     trainingFileList = listdir('trainingDigits')
     m = len(trainingFileList)
-    trainingMat = zeros(m,1024)
+    trainingMat = zeros((m, 1024))
     for i in range(m):
         fileNameStr = trainingFileList[i]
         fileStr = fileNameStr.split('.')[0]
         classNumStr = int(fileStr.split('_')[0])
         hwLabels.append(classNumStr)
-        trainingMat[i,:]=img2vector('trainingDigits/%s' % fileNameStr)
+        trainingMat[i, :] = img2vector('trainingDigits/%s' % fileNameStr)
     testFileList = listdir('testDigits')
     errorCount = 0.0
     mTest = len(testFileList)
     for i in range(mTest):
         fileNameStr = testFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        vectorUnderTest = img2vector('testDigits/%s' % fileNameStr)
+        classifierResult = classify0(vectorUnderTest, trainingMat, hwLabels, 3)
+        print "the classifier came back with: %d, the real answer is %d" % (classifierResult, classNumStr)
+        if (classifierResult != classNumStr): errorCount += 1.0
+    print "the total number of errors is: %d" % errorCount
+    print "the total error rate is: %f" % (errorCount / float(mTest))
+
+
+handwritingClassTest()
